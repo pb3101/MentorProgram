4a0d246ce1b967616f057ea1d33df678503b2fff
pb3101 <pavlo.balyuk@gmail.com>
2017-09-13 15:32:47 +0300

    Updated to packages structure

---
+++ src/Tasks3/CharsNumber.java
@@ -0,0 +1,29 @@
+package Tasks3; /**
+ * Created by pavlo.balyuk on 6/9/2017.
+ */
+
+import java.util.*;
+import java.lang.*;
+
+
+public class CharsNumber {
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("ВВедіть стрічку; ");
+        String sentense = input.next();
+        String newSentense = sentense.replaceAll(" ", "");
+        int size = 0;
+        int charCount = 0;
+        char temp;
+       /* char delim = ' ';*/
+        size = newSentense.length();
+        /**One more loop needed to go throw delimiter*/
+        for (int i = 0; i < size; i++) {
+            temp = newSentense.charAt(i);
+            if ( temp != ' ' )
+                charCount++;
+        }
+        System.out.print("Кількість букв у введеній стрічці = " + charCount);
+    }
+}
