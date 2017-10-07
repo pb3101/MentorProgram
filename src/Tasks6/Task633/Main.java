6f50689ada7269289711f0b42d1c7a5d959dbc9d
pb3101 <pavlo.balyuk@gmail.com>
2017-10-07 16:39:57 +0300

    Added Task 6.3 (example of Wrapper) and Task 631-633 (own implementation) Added packages to other Pattern exaplmes

---
+++ src/Tasks6/Task633/Main.java
@@ -0,0 +1,11 @@
+package Tasks6.Task633;
+
+/**
+ * Created by pavlo.balyuk on 10/7/2017.
+ */
+class Main {
+    public static void main(String args[]){
+        MyString myString = new MyString();
+        System.out.println(myString.capitalyzeSymbols("lowercase"));
+    }
+}
